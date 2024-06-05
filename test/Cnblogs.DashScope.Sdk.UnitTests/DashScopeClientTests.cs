93a859d88df1f3de322d732c639eded3f8514a8b
沈星繁 <ikesnowy@outlook.com>
2024-06-05 15:24:29 +0800

    fix: allow configuring timeout with DashScopeClient

--- test/Cnblogs.DashScope.Sdk.UnitTests/DashScopeClientTests.cs
+++ test/Cnblogs.DashScope.Sdk.UnitTests/DashScopeClientTests.cs
@@ -1,4 +1,5 @@
-﻿using Cnblogs.DashScope.Core;
+﻿using System.Reflection;
+using Cnblogs.DashScope.Core;
 using FluentAssertions;
 
 namespace Cnblogs.DashScope.Sdk.UnitTests;
@@ -17,4 +18,72 @@ public class DashScopeClientTests
         // Assert
         act.Should().NotThrow();
     }
+
+    [Theory]
+    [MemberData(nameof(ParamsShouldNotCache))]
+    public void DashScopeClient_Constructor_NotCacheableParams(
+        string apiKey,
+        string apiKey2,
+        TimeSpan? timeout,
+        TimeSpan? timeout2)
+    {
+        // Arrange
+        var client = new DashScopeClient(apiKey, timeout);
+        var client2 = new DashScopeClient(apiKey2, timeout2);
+
+        // Act
+        var value = HttpClientAccessor.GetValue(client);
+        var value2 = HttpClientAccessor.GetValue(client2);
+
+        // Assert
+        value.Should().NotBe(value2);
+    }
+
+    [Theory]
+    [MemberData(nameof(ParamsShouldCache))]
+    public void DashScopeClient_Constructor_CacheableParams(
+        string apiKey,
+        string apiKey2,
+        TimeSpan? timeout,
+        TimeSpan? timeout2)
+    {
+        // Arrange
+        var client = new DashScopeClient(apiKey, timeout);
+        var client2 = new DashScopeClient(apiKey2, timeout2);
+
+        // Act
+        var value = HttpClientAccessor.GetValue(client);
+        var value2 = HttpClientAccessor.GetValue(client2);
+
+        // Assert
+        value.Should().Be(value2);
+    }
+
+    public static TheoryData<string, string, TimeSpan?, TimeSpan?> ParamsShouldNotCache
+        => new()
+        {
+            { "apiKey", "apiKey2", null, null }, // same null timespan with different apikey
+            {
+                // same timespan with different apiKey
+                "apikey", "apiKey2", TimeSpan.FromMinutes(2), TimeSpan.FromMinutes(2)
+            },
+            {
+                // same apikey with different timeout
+                "apiKey", "apiKey", TimeSpan.FromMinutes(2), TimeSpan.FromMinutes(1)
+            }
+        };
+
+    public static TheoryData<string, string, TimeSpan?, TimeSpan?> ParamsShouldCache
+        => new()
+        {
+            { "apiKey", "apiKey", null, null }, // same null timespan with same apikey
+            {
+                // same timespan with same apiKey
+                "apiKey", "apiKey", TimeSpan.FromMinutes(2), TimeSpan.FromMinutes(2)
+            }
+        };
+
+    private static readonly FieldInfo HttpClientAccessor = typeof(DashScopeClientCore).GetField(
+        "_httpClient",
+        BindingFlags.Instance | BindingFlags.NonPublic)!;
 }
