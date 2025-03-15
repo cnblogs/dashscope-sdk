1e23e967122bf80a92287328cfd748a44423186d
沈星繁 <ikesnowy@outlook.com>
2025-03-15 23:39:23 +0800

    feat: implement application call api

---
+++ src/Cnblogs.DashScope.Core/ApplicationRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace Cnblogs.DashScope.Core;
+
+/// <summary>
+/// Request body for an application all.
+/// </summary>
+public class ApplicationRequest
+{
+    /// <summary>
+    /// Content of this call.
+    /// </summary>
+    public required ApplicationInput Input { get; init; }
+
+    /// <summary>
+    /// Optional configurations.
+    /// </summary>
+    public required ApplicationParameters? Parameters { get; init; }
+}
