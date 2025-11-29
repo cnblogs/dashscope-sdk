363d14f8bf94477beff90ea8a17b2e7d90307acd
沈星繁 <ikesnowy@outlook.com>
2025-11-29 19:28:28 +0800

    feat: add table parsing sample

--- README.zh-Hans.md
+++ README.zh-Hans.md
@@ -2737,6 +2737,270 @@ Usage: in(524)/out(65)/image(310)/total(589)
 */
 ````
 
+##### 表格解析
+
+设置 `Parameters.OcrOptions.Task` 为 `table_parsing` 即可调用该内置任务，不需要传入额外的文字信息。
+
+该任务会识读图片中的表格并返回 HTML 格式的表格。
+
+示例：
+
+```csharp
+await using var file = File.OpenRead("table.jpg");
+var ossLink = await client.UploadTemporaryFileAsync("qwen-vl-ocr-latest", file, "table.jpg");
+Console.WriteLine($"File uploaded: {ossLink}");
+var messages =
+    new List<MultimodalMessage> { MultimodalMessage.User([MultimodalMessageContent.ImageContent(ossLink)]) };
+var completion = await client.GetMultimodalGenerationAsync(
+    new ModelRequest<MultimodalInput, IMultimodalParameters>()
+    {
+        Model = "qwen-vl-ocr-latest",
+        Input = new MultimodalInput { Messages = messages },
+        Parameters = new MultimodalParameters()
+        {
+            OcrOptions = new MultimodalOcrOptions()
+            {
+                Task = "table_parsing",
+            }
+        }
+    });
+
+Console.WriteLine(completion.Output.Choices[0].Message.Content[0].Text);
+```
+
+返回的内容（注意最外层会包含一个 markdown 代码块标记）：
+
+````markdown
+```html
+<table>
+  <tr>
+    <td>Record of test data</td>
+  </tr>
+  <tr>
+    <td>Project name：2B</td>
+    <td>Control No.CEPRI-D-JS1-JS-057-2022-003</td>
+  </tr>
+  <tr>
+    <td>Case name</td>
+    <td>Test No.3 Conductor rupture GL+GR(max angle)</td>
+    <td>Last load grade：</td>
+    <td>0%</td>
+    <td>Current load grade：</td>
+  </tr>
+  <tr>
+    <td>Measure</td>
+    <td>Load point</td>
+    <td>Load method</td>
+    <td>Actual Load(%)</td>
+    <td>Actual Load(kN)</td>
+  </tr>
+  <tr>
+    <td>channel</td>
+    <td>V1</td>
+    <td>活载荷</td>
+    <td>147.95</td>
+    <td>0.815</td>
+  </tr>
+  <tr>
+    <td>V03</td>
+    <td>V2</td>
+    <td>活载荷</td>
+    <td>111.75</td>
+    <td>0.615</td>
+  </tr>
+  <tr>
+    <td>V04</td>
+    <td>V3</td>
+    <td>活载荷</td>
+    <td>9.74</td>
+    <td>1.007</td>
+  </tr>
+  <tr>
+    <td>V05</td>
+    <td>V4</td>
+    <td>活载荷</td>
+    <td>7.88</td>
+    <td>0.814</td>
+  </tr>
+  <tr>
+    <td>V06</td>
+    <td>V5</td>
+    <td>活载荷</td>
+    <td>8.11</td>
+    <td>0.780</td>
+  </tr>
+  <tr>
+    <td>V07</td>
+    <td>V6</td>
+    <td>活载荷</td>
+    <td>8.54</td>
+    <td>0.815</td>
+  </tr>
+  <tr>
+    <td>V08</td>
+    <td>V7</td>
+    <td>活载荷</td>
+    <td>6.77</td>
+    <td>0.700</td>
+  </tr>
+  <tr>
+    <td>V09</td>
+    <td>V8</td>
+    <td>活载荷</td>
+    <td>8.59</td>
+    <td>0.888</td>
+  </tr>
+  <tr>
+    <td>L01</td>
+    <td>L1</td>
+    <td>活载荷</td>
+    <td>13.33</td>
+    <td>3.089</td>
+  </tr>
+  <tr>
+    <td>L02</td>
+    <td>L2</td>
+    <td>活载荷</td>
+    <td>9.69</td>
+    <td>2.247</td>
+  </tr>
+  <tr>
+    <td>L03</td>
+    <td>L3</td>
+    <td></td>
+    <td>2.96</td>
+    <td>1.480</td>
+  </tr>
+  <tr>
+    <td>L04</td>
+    <td>L4</td>
+    <td></td>
+    <td>3.40</td>
+    <td>1.700</td>
+  </tr>
+  <tr>
+    <td>L05</td>
+    <td>L5</td>
+    <td></td>
+    <td>2.45</td>
+    <td>1.224</td>
+  </tr>
+  <tr>
+    <td>L06</td>
+    <td>L6</td>
+    <td></td>
+    <td>2.01</td>
+    <td>1.006</td>
+  </tr>
+  <tr>
+    <td>L07</td>
+    <td>L7</td>
+    <td></td>
+    <td>2.38</td>
+    <td>1.192</td>
+  </tr>
+  <tr>
+    <td>L08</td>
+    <td>L8</td>
+    <td></td>
+    <td>2.10</td>
+    <td>1.050</td>
+  </tr>
+  <tr>
+    <td>T01</td>
+    <td>T1</td>
+    <td>活载荷</td>
+    <td>25.29</td>
+    <td>3.073</td>
+  </tr>
+  <tr>
+    <td>T02</td>
+    <td>T2</td>
+    <td>活载荷</td>
+    <td>27.39</td>
+    <td>3.327</td>
+  </tr>
+  <tr>
+    <td>T03</td>
+    <td>T3</td>
+    <td>活载荷</td>
+    <td>8.03</td>
+    <td>2.543</td>
+  </tr>
+  <tr>
+    <td>T04</td>
+    <td>T4</td>
+    <td>活载荷</td>
+    <td>11.19</td>
+    <td>3.542</td>
+  </tr>
+  <tr>
+    <td>T05</td>
+    <td>T5</td>
+    <td>活载荷</td>
+    <td>11.34</td>
+    <td>3.592</td>
+  </tr>
+  <tr>
+    <td>T06</td>
+    <td>T6</td>
+    <td>活载荷</td>
+    <td>16.47</td>
+    <td>5.217</td>
+  </tr>
+  <tr>
+    <td>T07</td>
+    <td>T7</td>
+    <td>活载荷</td>
+    <td>11.05</td>
+    <td>3.498</td>
+  </tr>
+  <tr>
+    <td>T08</td>
+    <td>T8</td>
+    <td>活载荷</td>
+    <td>8.66</td>
+    <td>2.743</td>
+  </tr>
+  <tr>
+    <td>T09</td>
+    <td>WT1</td>
+    <td>活载荷</td>
+    <td>36.56</td>
+    <td>2.365</td>
+  </tr>
+  <tr>
+    <td>T10</td>
+    <td>WT2</td>
+    <td>活载荷</td>
+    <td>24.55</td>
+    <td>2.853</td>
+  </tr>
+  <tr>
+    <td>T11</td>
+    <td>WT3</td>
+    <td>活载荷</td>
+    <td>38.06</td>
+    <td>4.784</td>
+  </tr>
+  <tr>
+    <td>T12</td>
+    <td>WT4</td>
+    <td>活载荷</td>
+    <td>37.70</td>
+    <td>5.030</td>
+  </tr>
+  <tr>
+    <td>T13</td>
+    <td>WT5</td>
+    <td>活载荷</td>
+    <td>30.48</td>
+    <td>4.524</td>
+  </tr>
+</table>
+```
+````
+
 
 
 ## 语音合成
---
+++ sample/Cnblogs.DashScope.Sample/Multimodal/OcrTableParsingSample.cs
@@ -0,0 +1,278 @@
+﻿using System.Text.Json;
+using Cnblogs.DashScope.Core;
+
+namespace Cnblogs.DashScope.Sample.Multimodal;
+
+public class OcrTableParsingSample : ISample
+{
+    /// <inheritdoc />
+    public string Description => "OCR table parsing sample";
+
+    /// <inheritdoc />
+    public async Task RunAsync(IDashScopeClient client)
+    {
+        // upload file
+        await using var file = File.OpenRead("table.jpg");
+        var ossLink = await client.UploadTemporaryFileAsync("qwen-vl-ocr-latest", file, "table.jpg");
+        Console.WriteLine($"File uploaded: {ossLink}");
+        var messages =
+            new List<MultimodalMessage> { MultimodalMessage.User([MultimodalMessageContent.ImageContent(ossLink)]) };
+        var completion = await client.GetMultimodalGenerationAsync(
+            new ModelRequest<MultimodalInput, IMultimodalParameters>()
+            {
+                Model = "qwen-vl-ocr-latest",
+                Input = new MultimodalInput { Messages = messages },
+                Parameters = new MultimodalParameters()
+                {
+                    OcrOptions = new MultimodalOcrOptions()
+                    {
+                        Task = "table_parsing",
+                    }
+                }
+            });
+
+        Console.WriteLine("HTML:");
+        Console.WriteLine(completion.Output.Choices[0].Message.Content[0].Text);
+
+        if (completion.Usage != null)
+        {
+            var usage = completion.Usage;
+            Console.WriteLine(
+                $"Usage: in({usage.InputTokens})/out({usage.OutputTokens})/image({usage.ImageTokens})/total({usage.TotalTokens})");
+        }
+    }
+}
+
+/*
+File uploaded: oss://dashscope-instant/52afe077fb4825c6d74411758cb1ab98/2025-11-29/9a7188f6-25ed-437b-8268-e95da646bdcf/table.jpg
+HTML:
+```html
+<table>
+  <tr>
+    <td>Record of test data</td>
+  </tr>
+  <tr>
+    <td>Project name：2B</td>
+    <td>Control No.CEPRI-D-JS1-JS-057-2022-003</td>
+  </tr>
+  <tr>
+    <td>Case name</td>
+    <td>Test No.3 Conductor rupture GL+GR(max angle)</td>
+    <td>Last load grade：</td>
+    <td>0%</td>
+    <td>Current load grade：</td>
+  </tr>
+  <tr>
+    <td>Measure</td>
+    <td>Load point</td>
+    <td>Load method</td>
+    <td>Actual Load(%)</td>
+    <td>Actual Load(kN)</td>
+  </tr>
+  <tr>
+    <td>channel</td>
+    <td>V1</td>
+    <td>活载荷</td>
+    <td>147.95</td>
+    <td>0.815</td>
+  </tr>
+  <tr>
+    <td>V03</td>
+    <td>V2</td>
+    <td>活载荷</td>
+    <td>111.75</td>
+    <td>0.615</td>
+  </tr>
+  <tr>
+    <td>V04</td>
+    <td>V3</td>
+    <td>活载荷</td>
+    <td>9.74</td>
+    <td>1.007</td>
+  </tr>
+  <tr>
+    <td>V05</td>
+    <td>V4</td>
+    <td>活载荷</td>
+    <td>7.88</td>
+    <td>0.814</td>
+  </tr>
+  <tr>
+    <td>V06</td>
+    <td>V5</td>
+    <td>活载荷</td>
+    <td>8.11</td>
+    <td>0.780</td>
+  </tr>
+  <tr>
+    <td>V07</td>
+    <td>V6</td>
+    <td>活载荷</td>
+    <td>8.54</td>
+    <td>0.815</td>
+  </tr>
+  <tr>
+    <td>V08</td>
+    <td>V7</td>
+    <td>活载荷</td>
+    <td>6.77</td>
+    <td>0.700</td>
+  </tr>
+  <tr>
+    <td>V09</td>
+    <td>V8</td>
+    <td>活载荷</td>
+    <td>8.59</td>
+    <td>0.888</td>
+  </tr>
+  <tr>
+    <td>L01</td>
+    <td>L1</td>
+    <td>活载荷</td>
+    <td>13.33</td>
+    <td>3.089</td>
+  </tr>
+  <tr>
+    <td>L02</td>
+    <td>L2</td>
+    <td>活载荷</td>
+    <td>9.69</td>
+    <td>2.247</td>
+  </tr>
+  <tr>
+    <td>L03</td>
+    <td>L3</td>
+    <td></td>
+    <td>2.96</td>
+    <td>1.480</td>
+  </tr>
+  <tr>
+    <td>L04</td>
+    <td>L4</td>
+    <td></td>
+    <td>3.40</td>
+    <td>1.700</td>
+  </tr>
+  <tr>
+    <td>L05</td>
+    <td>L5</td>
+    <td></td>
+    <td>2.45</td>
+    <td>1.224</td>
+  </tr>
+  <tr>
+    <td>L06</td>
+    <td>L6</td>
+    <td></td>
+    <td>2.01</td>
+    <td>1.006</td>
+  </tr>
+  <tr>
+    <td>L07</td>
+    <td>L7</td>
+    <td></td>
+    <td>2.38</td>
+    <td>1.192</td>
+  </tr>
+  <tr>
+    <td>L08</td>
+    <td>L8</td>
+    <td></td>
+    <td>2.10</td>
+    <td>1.050</td>
+  </tr>
+  <tr>
+    <td>T01</td>
+    <td>T1</td>
+    <td>活载荷</td>
+    <td>25.29</td>
+    <td>3.073</td>
+  </tr>
+  <tr>
+    <td>T02</td>
+    <td>T2</td>
+    <td>活载荷</td>
+    <td>27.39</td>
+    <td>3.327</td>
+  </tr>
+  <tr>
+    <td>T03</td>
+    <td>T3</td>
+    <td>活载荷</td>
+    <td>8.03</td>
+    <td>2.543</td>
+  </tr>
+  <tr>
+    <td>T04</td>
+    <td>T4</td>
+    <td>活载荷</td>
+    <td>11.19</td>
+    <td>3.542</td>
+  </tr>
+  <tr>
+    <td>T05</td>
+    <td>T5</td>
+    <td>活载荷</td>
+    <td>11.34</td>
+    <td>3.592</td>
+  </tr>
+  <tr>
+    <td>T06</td>
+    <td>T6</td>
+    <td>活载荷</td>
+    <td>16.47</td>
+    <td>5.217</td>
+  </tr>
+  <tr>
+    <td>T07</td>
+    <td>T7</td>
+    <td>活载荷</td>
+    <td>11.05</td>
+    <td>3.498</td>
+  </tr>
+  <tr>
+    <td>T08</td>
+    <td>T8</td>
+    <td>活载荷</td>
+    <td>8.66</td>
+    <td>2.743</td>
+  </tr>
+  <tr>
+    <td>T09</td>
+    <td>WT1</td>
+    <td>活载荷</td>
+    <td>36.56</td>
+    <td>2.365</td>
+  </tr>
+  <tr>
+    <td>T10</td>
+    <td>WT2</td>
+    <td>活载荷</td>
+    <td>24.55</td>
+    <td>2.853</td>
+  </tr>
+  <tr>
+    <td>T11</td>
+    <td>WT3</td>
+    <td>活载荷</td>
+    <td>38.06</td>
+    <td>4.784</td>
+  </tr>
+  <tr>
+    <td>T12</td>
+    <td>WT4</td>
+    <td>活载荷</td>
+    <td>37.70</td>
+    <td>5.030</td>
+  </tr>
+  <tr>
+    <td>T13</td>
+    <td>WT5</td>
+    <td>活载荷</td>
+    <td>30.48</td>
+    <td>4.524</td>
+  </tr>
+</table>
+```
+Usage: in(2731)/out(1877)/image(2657)/total(4608)
+ */
