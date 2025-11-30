808caac6a7950541bf58957d9d7b490d7709d6d5
沈星繁 <ikesnowy@outlook.com>
2025-11-30 21:53:21 +0800

    feat: support upload files api

---
+++ src/Cnblogs.DashScope.Core/DashScopeUploadFileData.cs
@@ -0,0 +1,10 @@
+﻿namespace Cnblogs.DashScope.Core;
+
+/// <summary>
+/// Represents upload file result data.
+/// </summary>
+/// <param name="UploadedFiles">Successful uploads.</param>
+/// <param name="FailedUploads">Failed uploads.</param>
+public record DashScopeUploadFileData(
+    List<DashScopeUploadedFileRecord> UploadedFiles,
+    List<DashScopeFailedUploadRecord> FailedUploads);
