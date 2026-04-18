a0ca6575110bc56675d27d5b25e90c815262968c
沈星繁 <ikesnowy@outlook.com>
2026-04-18 20:55:24 +0800

    docs: add batch sample and update README.md

---
+++ sample/Cnblogs.DashScope.Sample/BatchesSample.cs
@@ -0,0 +1,15 @@
+﻿using Cnblogs.DashScope.Core;
+
+namespace Cnblogs.DashScope.Sample;
+
+public abstract class BatchesSample : ISample
+{
+    /// <inheritdoc />
+    public string Group => "Batches";
+
+    /// <inheritdoc />
+    public abstract string Description { get; }
+
+    /// <inheritdoc />
+    public abstract Task RunAsync(IDashScopeClient client);
+}
